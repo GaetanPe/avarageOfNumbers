0ccb13ccc95c599829a558abef84319d898378c4
GaetanPe <gaetan.peronnet@gmail.com>
2022-03-16 16:50:25 +0100

    Avarage of numbers

---
+++ moyenneNombres/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace moyenneNombres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float firstNumber, secondNumber, thirdNumber, calculationOfTheAverage;
+
+            Console.Write("Input a number: ");
+            firstNumber = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Input a second number: ");
+            secondNumber = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Input a third number: ");
+            thirdNumber = Convert.ToSingle(Console.ReadLine());
+
+            calculationOfTheAverage = (firstNumber + secondNumber + thirdNumber) / 3;
+            Console.WriteLine("average of numbers= " + calculationOfTheAverage);
+
+        }
+    }
+}
